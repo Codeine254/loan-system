876e2cb13a0e7d42b619e801016c45753e0f7338
Moses Wekesa <103812725+Codeine254@users.noreply.github.com>
2023-09-01 15:21:59 +0300

    Add files via upload

---
+++ loan-sys/loan_system/Source_Code/admin/about_author.php
@@ -0,0 +1,25 @@
+<?php include 'header.php';?>
+    
+    <div class="login-area">
+        <div class="container">
+            
+            	<form style="width: 850px;""> 
+        <div style="font-size: 20px; padding: 5%;">
+        Name : <b>Mayuri K.</b><br>
+				Email : <b>juniorcode039@outlook.com</b><br>
+				Other Projects : <b> <a href="https://cutt.ly/KgPYrnS" target="_blank">Click Here to View</a>
+
+				</b><br>
+				<br>
+				Experience : <b>I am a Programmer having Experty in languages like PHP, Codeignitor and Laravel. having 5 years+ Experience. I love to work with programming and various logics behind it.</b><br><br>
+				About Work : <b>
+Till Date I have Developed many Projects For students or Professional Software Development, Website Development at affordable cost.</b><br>
+				
+
+    </form>
+
+            </div>
+        </div>
+    </div>
+    
+    <?php include('footer.php');?>
